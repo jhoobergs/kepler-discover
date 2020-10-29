ca4e66de6e6506fbbdc49dcfb51a003dba6b5015
Jesse Hoobergs <jesse@codebergs.com>
2020-10-29 18:48:26 +0100

    Fix folder?

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -2,5 +2,5 @@ from subprocess import Popen
 
 
 def load_jupyter_server_extension(nbapp):
-    """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
+    """serve the jupiter_notebook directory with bokeh server"""
+    Popen(["bokeh", "serve", "jupiter_notebook", "--allow-websocket-origin=*"])
